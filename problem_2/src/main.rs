b23cb246fea1f1b81cdccf7601b867850c21e071
Nico Piderman <nico.piderman@gmail.com>
2023-01-08 11:56:07 +0100

    save

--- problem_2/src/main.rs
+++ problem_2/src/main.rs
@@ -23,13 +23,12 @@ enum Message {
 
 impl Message {
     pub fn cast(bytes: [u8; 9]) -> Message {
-        println!("casting bytes: {:?}", bytes);
         match bytes[0] {
             73 => Message::Insert {
                 timestamp: Self::parse_int(&bytes[1..5]),
                 price: Self::parse_int(&bytes[5..9]),
             },
-            51 => Message::Query {
+            81 => Message::Query {
                 mintime: Self::parse_int(&bytes[1..5]),
                 maxtime: Self::parse_int(&bytes[5..9]),
             },
@@ -46,33 +45,29 @@ async fn handle_stream(stream: TcpStream) -> io::Result<()> {
     let mut db: BTreeMap<i32, i32> = BTreeMap::new();
     let mut reader = BufReader::new(stream);
     let mut message_bytes: [u8; 9] = [0; 9];
-    while let Ok(num_bytes) = reader.read(&mut message_bytes).await {
-        println!("read {} bytes", num_bytes);
-        if num_bytes == 0 {
-            break;
-        }
+    while let Ok(_num_bytes) = reader.read_exact(&mut message_bytes).await {
         match Message::cast(message_bytes) {
             Message::Insert { price, timestamp } => {
-                println!("insert {} @ {}", price, timestamp);
+                println!("{:?} - insert {} @ {}", message_bytes, price, timestamp);
                 db.insert(timestamp, price);
                 ()
             }
             Message::Query { mintime, maxtime } => {
-                println!("query {} to {}", mintime, maxtime);
+                println!("{:?} - query {} to {}", message_bytes, mintime, maxtime);
                 let mut count: i32 = 0;
                 let mut sum: i32 = 0;
                 for (_timestamp, &amount) in db.range((Included(mintime), Included(maxtime))) {
-                    println!("ITERATE");
                     count = count + 1;
                     sum = sum + amount;
                 }
                 let mean = if count > 0 { sum / count } else { 0 };
-                println!("sum {}, count {}, mean {}", sum, count, mean);
+                println!("query result: sum {}, count {}, mean {}", sum, count, mean);
                 reader.write(&mean.to_be_bytes()).await?;
                 ()
             }
             Message::Invalid => {
-                println!("got invalid message");
+                println!("got invalid message: {:?}", message_bytes);
+                message_bytes = [0; 9];
                 ()
             }
         };
--- problem_2/test.exs
+++ problem_2/test.exs
@@ -1,10 +1,34 @@
-{:ok, socket} = :gen_tcp.connect('localhost', 7878, mode: :binary, active: false)
+# address = '137.66.59.170'
+address = 'localhost'
+{:ok, socket} = :gen_tcp.connect(address, 7878, mode: :binary, active: false)
 
+# query
+:gen_tcp.send(socket, <<81, 0, 0, 0, 0, 1, 0, 0, 103>>)
+
+{:ok, <<0, 0, 0, 0>>} = :gen_tcp.recv(socket, 0)
+
+# insert
 :gen_tcp.send(socket, <<73, 0, 0, 48, 57, 0, 0, 0, 101>>)
 :gen_tcp.send(socket, <<73, 0, 0, 48, 58, 0, 0, 0, 102>>)
 :gen_tcp.send(socket, <<73, 0, 0, 48, 59, 0, 0, 0, 103>>)
-# query
-:gen_tcp.send(socket, <<51, 0, 0, 0, 0, 1, 0, 0, 103>>)
 
-:gen_tcp.recv(socket, 9)
-|> dbg()
+# query
+:gen_tcp.send(socket, <<81, 0, 0, 0, 0, 1, 0, 0, 103>>)
+
+{:ok, <<0, 0, 0, 102>>} = :gen_tcp.recv(socket, 0)
+
+for i <- 59..65, do: :gen_tcp.send(socket, <<73, 0, 0, 48, i, 0, 0, 0, 210>>)
+
+# query
+:gen_tcp.send(socket, <<81, 0, 0, 0, 0, 1, 0, 0, 103>>)
+
+{:ok, <<0, 0, 0, 185>>} = :gen_tcp.recv(socket, 0)
+
+# invalid message
+:gen_tcp.send(socket, <<1, 0, 0, 48, 0, 0, 0, 64, 0>>)
+
+# query
+:gen_tcp.send(socket, <<81, 0, 0, 0, 0, 1, 0, 0, 103>>)
+
+{:ok, <<0, 0, 0, 185>>} = :gen_tcp.recv(socket, 0)
+# {:error, :closed} = :gen_tcp.recv(socket, 0)
