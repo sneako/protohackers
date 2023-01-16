ce4871eb94bfae2551a561656e5c62df761b40f8
Nico Piderman <nico.piderman@gmail.com>
2023-01-16 17:18:19 +0100

    getting close...

--- problem_5/src/main.rs
+++ problem_5/src/main.rs
@@ -1,11 +1,14 @@
+use lazy_static::lazy_static;
 use log::{error, info};
+use regex::Regex;
 use std::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 // use tokio::net::tcp::{ReadHalf, WriteHalf};
 
 const UPSTREAM: &str = "chat.protohackers.com:16963";
-// static TONY_ADDR: &str = "7YWHMfk9JZe0LM0g1ZauHuiSxhI";
+// const UPSTREAM: &str = "localhost:7879";
+const TONY_ADDR: &str = "7YWHMfk9JZe0LM0g1ZauHuiSxhI";
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -29,11 +32,11 @@ async fn handle_stream(mut downstream: TcpStream) -> io::Result<()> {
     // proxy the welcome message from the upstream
     match upstream_rx.read(&mut upstream_buf).await {
         Ok(num_bytes) => {
+            info!("welcome");
             info!("upstream -> downstream: welcome msg, {} bytes", num_bytes);
             downstream_tx
                 .write_all(&mut upstream_buf[..num_bytes])
-                .await?;
-            info!("sent welcome")
+                .await?
         }
         error => error!("failed to read welcome message: {:?}", error),
     }
@@ -41,11 +44,12 @@ async fn handle_stream(mut downstream: TcpStream) -> io::Result<()> {
     // proxy the name
     match downstream_rx.read(&mut downstream_buf).await {
         Ok(num_bytes) => {
-            info!("downstream -> upstream: name, {} bytes", num_bytes);
-            upstream_tx
-                .write_all(&mut downstream_buf[..num_bytes])
-                .await?;
-            info!("sent name");
+            if num_bytes > 0 {
+                info!("downstream -> upstream: name, {} bytes", num_bytes);
+                upstream_tx
+                    .write_all(&mut downstream_buf[..num_bytes])
+                    .await?
+            }
         }
         _ => {
             error!("failed reading the name!");
@@ -55,29 +59,32 @@ async fn handle_stream(mut downstream: TcpStream) -> io::Result<()> {
     // proxy the room contains message
     match upstream_rx.read(&mut upstream_buf).await {
         Ok(num_bytes) => {
-            info!(
-                "upstream -> downstream: room contains msg, {} bytes",
-                num_bytes
-            );
-            downstream_tx
-                .write_all(&mut upstream_buf[..num_bytes])
-                .await?;
-            info!("sent room contains")
+            if num_bytes > 0 {
+                info!("joined");
+                info!(
+                    "upstream -> downstream: room contains msg, {} bytes",
+                    num_bytes
+                );
+                downstream_tx
+                    .write_all(&mut upstream_buf[..num_bytes])
+                    .await?
+            }
         }
         error => error!("failed reading room contains message: {:?}", error),
     }
 
     // chat loop
     loop {
-        info!("looop");
         tokio::select! {
             // listen for downstream
             Ok(num_bytes) = downstream_rx.read(&mut downstream_buf) => {
+                if num_bytes == 0 {
+                    break;
+                }
                 info!("upstream <- downstream: message {} bytes", num_bytes);
-                // proxy the line upstream
-                upstream_tx
-                    .write_all(&mut downstream_buf[..num_bytes])
-                    .await?;
+                let string = String::from_utf8_lossy(&downstream_buf[..num_bytes]);
+                let updated = replace_coin_addrs(string.to_string());
+                upstream_tx.write_all(updated.as_bytes()).await?
             }
             // listen for upstream
             Ok(num_bytes) = upstream_rx.read(&mut upstream_buf) => {
@@ -85,22 +92,64 @@ async fn handle_stream(mut downstream: TcpStream) -> io::Result<()> {
                     break;
                 }
                 info!("upstream -> downstream: replies, {} bytes", num_bytes);
-                // send everything the upstream sent to the downstream
-                // downstream.send_all(&mut upstream_buffer[..read_bytes]);
+                let string = String::from_utf8_lossy(&downstream_buf[..num_bytes]);
+                let updated = replace_coin_addrs(string.to_string());
                 downstream_tx
-                    .write_all(&mut upstream_buf[..num_bytes])
-                    .await?;
-                ()
+                    .write_all(updated.as_bytes())
+                    .await?
             }
         }
     }
     io::Result::Ok(())
 }
 
+fn replace_coin_addrs(message: String) -> String {
+    lazy_static! {
+        static ref RE: Regex = Regex::new("(7[a-zA-Z0-9]+)").unwrap();
+    }
+
+    RE.find_iter(&message.to_string())
+        .fold(message, |response, possible| {
+            let pos = possible.as_str();
+            if pos.len() > 25 && pos.len() <= 36 {
+                response.replace(pos, TONY_ADDR)
+            } else {
+                response
+            }
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::replace_coin_addrs;
+
+    #[test]
+    fn replace_coin_addr_test() {
+        let input = "".to_string();
+        let expected = "".to_string();
+        assert_eq!(expected, replace_coin_addrs(input));
+
+        let input = "Hi alice, please send payment to 7iKDZEwPZSqIvDnHvVN2r0hUWXD5rHX".to_string();
+        let expected = "Hi alice, please send payment to 7YWHMfk9JZe0LM0g1ZauHuiSxhI".to_string();
+        assert_eq!(expected, replace_coin_addrs(input));
+
+        let input = "please send 750 coins to 7iKDZEwPZSqIvDnHvVN2r0hUWXD5rHX".to_string();
+        let expected = "please send 750 coins to 7YWHMfk9JZe0LM0g1ZauHuiSxhI".to_string();
+        assert_eq!(expected, replace_coin_addrs(input));
+
+        let input = "Send refunds to 7wLWEPxI9ta4H46wssWIc2FfGd please.".to_string();
+        let expected = "Send refunds to 7YWHMfk9JZe0LM0g1ZauHuiSxhI please.".to_string();
+        assert_eq!(expected, replace_coin_addrs(input));
+
+        let input = "Send refunds to 7QyB2Y1ZaKPCI3YBFdwjGv0jO6je7b please.".to_string();
+        let expected = "Send refunds to 7YWHMfk9JZe0LM0g1ZauHuiSxhI please.".to_string();
+        assert_eq!(expected, replace_coin_addrs(input))
+    }
+}
 // async fn proxy_message(
-//     mut from: ReadHalf<'_>,
-//     mut to: WriteHalf<'_>,
-//     mut buffer: Vec<u8>,
+//     mut from: ReadHalf,
+//     mut to: WriteHalf,
+//     &mut buffer: Vec<u8>,
 // ) -> io::Result<()> {
 //     match from.read(&mut buffer).await {
 //         Ok(num_bytes) => {
@@ -116,19 +165,4 @@ async fn handle_stream(mut downstream: TcpStream) -> io::Result<()> {
 //         error => error!("failed to read welcome message: {:?}", error),
 //     }
 //     io::Result::Ok(())
-// }
-// fn username_valid(input: &String) -> bool {
-//     let mut all_alphanumeric = true;
-//     let mut any_alphabetic = false;
-//
-//     input.chars().for_each(|c| {
-//         if !any_alphabetic && c.is_alphabetic() {
-//             any_alphabetic = true;
-//         }
-//
-//         if !c.is_alphanumeric() {
-//             all_alphanumeric = false;
-//         }
-//     });
-//     return all_alphanumeric && any_alphabetic;
 // }
